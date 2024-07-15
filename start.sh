481f921a20d79b248165d32ecbda87cb825939b4
Dylan Saffer <dylan.saffer@clowdertech.com>
2024-07-15 03:48:51 -0400

    more changes?

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the directory where your repository is located
-REPO_DIR="/root/agentkitten"
+REPO_DIR="/root/agent-kitten-v2"
 ENV_FILE="$REPO_DIR/.env"
 
 # Navigate to the directory
