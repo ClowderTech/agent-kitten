c78e23f62746fb475d1e3dc4ef51e91d61cf7018
Dylan Saffer <dylan.saffer@clowdertech.com>
2024-07-19 08:00:24 -0400

    test

--- helper.sh
+++ helper.sh
@@ -47,7 +47,7 @@ update_repo() {
     chmod +x "$SCRIPT_PATH"
 
     # Check if bun is installed, install it if not
-    if [ ! -d "$BUN_INSTALL_DIR" ]; then
+    if [ ! -d "$BUN_INSTALL" ]; then
         echo "Bun is not installed. Installing..."
         source "$HOME/.bashrc"
         curl -fsSL https://bun.sh/install | bash
