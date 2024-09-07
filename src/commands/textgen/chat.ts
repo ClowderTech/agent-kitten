892391baff72c7a54872a0863ec723761d7d105f
Dylan Saffer <me@woshidylan.dev>
2024-09-07 04:46:26 -0400

    chanage the model (again)

--- src/commands/textgen/chat.ts
+++ src/commands/textgen/chat.ts
@@ -393,7 +393,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 	const request: ChatRequest = {
 		// model: 'mixtral:8x7b',
 		// model: 'gpt-4o-mini',
-		model: "mistral-nemo",
+		model: "llama3.1",
 		messages: user_data.messages,
 		stream: false,
 		keep_alive: "15m",
