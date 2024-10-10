6c6f484aa514e816e3c25d3a631593082106f143
Dylan Saffer <me@woshidylan.dev>
2024-10-09 21:54:43 -0400

    keep alive to -1

--- src/commands/textgen/chat.ts
+++ src/commands/textgen/chat.ts
@@ -290,6 +290,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 
 				const { chat_response } = await chatWithFuncs(ollama, {
 					model: "moondream:1.8b",
+					keep_alive: "-1",
 					messages: [
 						{
 							role: "user",
@@ -362,7 +363,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 		model: "qwen2.5:7b",
 		messages: user_data.messages,
 		stream: false,
-		keep_alive: "15m",
+		keep_alive: "-1",
 		options: {
 			num_ctx: 16383,
 			num_predict: 4095,
