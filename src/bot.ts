d296eafc7ebe8d7643f29cd303721da7df5841a0
Dylan Saffer <me@mrscaryspacecat.dev>
2024-11-11 19:35:09 -0500

    fix smth 2

--- src/bot.ts
+++ src/bot.ts
@@ -83,7 +83,8 @@ client.moonlink = new Manager({
 	],
 	options: {},
 	sendPayload: (guildID: string, sPayload: string) => {
-		client.guilds.cache.get(guildID)!.shard.send(JSON.parse(sPayload));
+		const guild = client.guilds.cache.get(guildId);
+		if (guild) guild.shard.send(payload); // Sending data to the shard if the guild is available
 	},
 });
 
