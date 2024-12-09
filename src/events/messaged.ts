8ea459c7f6af026031a2253c27bae3e61874a450
Dylan Saffer <me@mrscaryspacecat.dev>
2024-12-08 22:59:32 -0500

    fix possible triggers for non rule breaks in automod

--- src/events/messaged.ts
+++ src/events/messaged.ts
@@ -126,7 +126,7 @@ export async function execute(message: Message) {
 				messages: [
 					{
 						role: "system",
-						content: `You are an AI Moderation bot. Your job is to moderate a discord server based on the set rules. If there is an offending message, state its Message ID and the reason for offending seperated by a semicolon and nothing else. If there are multiple offending messages, seperate them with a comma. Otherwise, say anything else. The rules are as follows:\n\n${rules}`,
+						content: `You are an AI Moderation bot. Your job is to moderate a discord server based on the set rules. If there is an offending message, state its Message ID and the reason for offending seperated by a semicolon and nothing else. If there are multiple offending messages, seperate them with a comma. If the message does not offend any rules, do not state its Message ID, Reason, any semicolon, or any comma. The rules are as follows:\n\n${rules}`,
 					},
 					{
 						role: "user",
