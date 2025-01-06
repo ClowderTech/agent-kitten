6c173c7e647e51ded586a64a0484e98eb92f310f
MrScarySpaceCat <dylan.saffer.27@gmail.com>
2025-01-06 11:27:16 +0000

    Update src/utils/leveling.ts

--- src/utils/leveling.ts
+++ src/utils/leveling.ts
@@ -132,7 +132,7 @@ export async function getUsersByExperienceRange(
 
 	// Sort and slice users
 	const sortedUsers = users.sort((a, b) =>
-		a.level === b.level ? a.level - b.level : a.level - b.level
+		b.level === a.level ? b.experience - a.experience : b.level - a.level
 	);
 	const slicedUsers = sortedUsers.slice(x, y + 1);
 
