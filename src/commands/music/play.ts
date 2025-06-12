ccd64d967001635117703e5a8a2e9399bc56b135
MrScarySpaceCat <me@mrscaryspacecat.dev>
2025-06-12 11:07:36 -0400

    update packages and shit or smth

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -49,7 +49,25 @@ export async function execute(
 	let player = client.moonlink.players.get(guildId);
 
 	if (!player) {
-		try {
+		player = client.moonlink.players.create({
+			guildId,
+			voiceChannelId: voiceChannel.id,
+			textChannelId: interaction.channel.id,
+			volume: 100,
+			autoPlay: false,
+			autoLeave: true,
+		});
+
+		if (!player) {
+			client.moonlink.nodes.cache.forEach(async (node) => {
+				const responding = await node.checkHealth();
+				if (!responding.responding || node.needsRestart()) {
+					node.reconnect();
+				}
+			});
+
+			setTimeout(function () {}, 3000);
+
 			player = client.moonlink.players.create({
 				guildId,
 				voiceChannelId: voiceChannel.id,
@@ -58,26 +76,6 @@ export async function execute(
 				autoPlay: false,
 				autoLeave: true,
 			});
-		} catch (error) {
-			if (error instanceof TypeError) {
-				client.moonlink.nodes.cache.forEach(async (node) => {
-					const responding = await node.checkHealth();
-					if (!responding.responding || node.needsRestart()) {
-						node.reconnect();
-					}
-				});
-
-				setTimeout(function () {}, 3000);
-
-				player = client.moonlink.players.create({
-					guildId,
-					voiceChannelId: voiceChannel.id,
-					textChannelId: interaction.channel.id,
-					volume: 100,
-					autoPlay: false,
-					autoLeave: true,
-				});
-			}
 		}
 	}
 
