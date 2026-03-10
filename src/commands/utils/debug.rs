6d103667fedd0a45bf2c4ce9679292863e5b77bf
MrScarySpaceCat <me@mrscaryspacecat.dev>
2026-03-10 11:12:44 -0400

    fix stupid version
    Signed-off-by: MrScarySpaceCat <me@mrscaryspacecat.dev>

--- src/commands/utils/debug.rs
+++ src/commands/utils/debug.rs
@@ -1,7 +1,9 @@
 use crate::{Context, Error};
 use chrono::Utc;
+use human_bytes::human_bytes;
 use poise::serenity_prelude as serenity;
 use serenity::builder::CreateEmbed;
+use sysinfo::{ProcessRefreshKind, ProcessesToUpdate};
 
 /// Convert milliseconds to the largest appropriate unit
 fn convert_millis_to_human_readable(millis: i64) -> String {
@@ -20,17 +22,6 @@ fn convert_millis_to_human_readable(millis: i64) -> String {
     "less than a second".into()
 }
 
-/// Format a byte count into KB/MB/etc.
-fn format_bytes(bytes: u64) -> String {
-    if bytes == 0 {
-        return "0 B".into();
-    }
-    let sizes = ["B", "KiB", "MiB", "GiB", "TiB"];
-    let i = (bytes as f64).log(1024.0).floor() as usize;
-    let val = bytes as f64 / 1024f64.powi(i as i32);
-    format!("{:.2} {}", val, sizes[i])
-}
-
 /// Get some information about the bot and how well it is performing
 #[poise::command(slash_command)]
 pub async fn debug(ctx: Context<'_>) -> Result<(), Error> {
@@ -44,15 +35,18 @@ pub async fn debug(ctx: Context<'_>) -> Result<(), Error> {
 
     use sysinfo::System;
     let mut sys = System::new_all();
-    let current_process_id = sysinfo::get_current_pid().expect("Failed to get current process ID");
     std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
-    sys.refresh_all();
+    sys.refresh_processes_specifics(
+        ProcessesToUpdate::All,
+        true,
+        ProcessRefreshKind::nothing().with_cpu(),
+    );
     let current_process = sys
-        .process(current_process_id)
+        .process(sysinfo::get_current_pid()?)
         .expect("Failed to get current process");
-    let heap = current_process.memory();
-    let memory_usage = format_bytes(heap);
-    let cpu_usage = current_process.cpu_usage() / 100.0;
+    let heap = current_process.virtual_memory();
+    let memory_usage = human_bytes(heap as f64);
+    let cpu_usage = current_process.cpu_usage();
     let shard_id = ctx.serenity_context().shard_id;
     let amount_users = ctx.serenity_context().cache.user_count();
     let amount_guilds = ctx.serenity_context().cache.guild_count();
@@ -86,7 +80,7 @@ pub async fn debug(ctx: Context<'_>) -> Result<(), Error> {
         .field("Guild Count", amount_guilds.to_string(), true)
         .field("Current Shard ID", shard_id.to_string(), true)
         .field("Memory Usage", memory_usage, true)
-        .field("CPU Usage", format!("{cpu_usage} Cores"), true);
+        .field("CPU Usage", format!("{} Cores", cpu_usage / 100.0), true);
 
     ctx.send(poise::CreateReply::default().embed(embed)).await?;
 
